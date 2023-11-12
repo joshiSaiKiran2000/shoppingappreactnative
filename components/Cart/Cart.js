57eac9b358e08f06e970dad0ace4ef49592216fc
joshiSaiKiran2000 <joshisaikiran2000@gmail.com>
2023-11-13 04:09:18 +0530

    cat items

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -2,25 +2,45 @@ import { FlatList, StyleSheet, Text, View, Button, Image } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Cart = () => {
-  const cartState = useSelector((state) => state.cart); 
-  
+    const cartState = useSelector((state) => state.cart);
 
-  return (
-    <View>
-      <FlatList
-        data={cartState.cartItems} 
-        renderItem={({ item }) => {
-          const { title } = item;
-          return (
-            <View>
-              <Text>{title}</Text>
-            </View>
-          );
-        }}
-        keyExtractor={(item) => item.id.toString()} 
-      />
-    </View>
-  );
+
+    return (
+        <View>
+            <FlatList
+                data={cartState.cartItems}
+                renderItem={({ item }) => {
+                    const { image, title, price } = item;
+                    return (
+                        <View style={styles.cartstyle}>
+                            <Image style={{ width: 50, height: 50 }}
+                                source={{ uri: `${image}` }} />
+                            <View>
+
+                                <Text>{title}</Text>
+                                <Text>${price}</Text>
+                            </View>
+                        </View>
+                    );
+                }}
+                keyExtractor={(item) => item.id.toString()}
+            />
+        </View>
+    );
 };
 
+
+
+const styles = StyleSheet.create({
+    cartstyle:{
+       
+        flexDirection:"row",
+        
+        alignItems:"center",
+        textAlign:"center"
+        
+
+      }
+     
+  });
 export default Cart;
